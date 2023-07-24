22ef798a37accf17253b6e07732261d980500e63
macray <macraykorir254@gmail.com>
2023-07-24 12:13:52 +0300

    Sort Search Bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,21 +1,22 @@
 import React, { useState } from 'react';
 
-const SearchBar = () => {
-  const [searchTerm, setSearchTerm] = useState('');
+const SearchBar = ({ onSort }) => {
+  const [sortBy, setSortBy] = useState('');
 
-  const handleChange = (e) => {
-    setSearchTerm(e.target.value);
-    // TODO: Filter transactions and pass the filtered transactions to the TransactionTable (core deliverable)
+  const handleSortChange = (event) => {
+    const selectedSort = event.target.value;
+    setSortBy(selectedSort);
+    onSort(selectedSort); // Pass the selected sort value to the parent component
   };
 
   return (
     <div>
-      <input
-        type="text"
-        placeholder="Search transactions by description..."
-        value={searchTerm}
-        onChange={handleChange}
-      />
+      <label>Sort By: </label>
+      <select value={sortBy} onChange={handleSortChange}>
+        <option value="">Select...</option>
+        <option value="category">Category</option>
+        <option value="description">Description</option>
+      </select>
     </div>
   );
 };
